6c6cda012c5b51bf47b636be5a19b9d6cd546baa
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-11-10 20:59:30 +0300

    fix: fixing BookList component

--- frontend/src/components/bookList/BookList.tsx
+++ frontend/src/components/bookList/BookList.tsx
@@ -3,12 +3,17 @@ import { Book } from 'components';
 import s from './BookList.module.css';
 
 export const BookList = () => {
-  const { books } = useBook();
+  const { books, removeBook } = useBook();
 
   return (
     <div className={s.bookList}>
       {books.map((book, index) => (
-        <Book key={book.id} number={++index} book={book} />
+        <Book
+          key={book.id}
+          number={++index}
+          book={book}
+          deleteBook={removeBook}
+        />
       ))}
     </div>
   );
