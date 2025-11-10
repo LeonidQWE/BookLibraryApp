ee96bfee5dfa037a1bdee4bab6d1ff02c4974524
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-11-10 20:31:28 +0300

    feat: update Main component

--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,11 +1,17 @@
-import { BookForm, Container } from 'components';
+import { BookForm, BookList, Container } from 'components';
+import s from './Main.module.css';
 
 export const Main = () => {
   return (
     <main>
       <Container>
-        <div>
+        <div className={s.content}>
           <BookForm />
+
+          <div className={s.description}>
+            {/* BookFilter */}
+            <BookList />
+          </div>
         </div>
       </Container>
     </main>
