3c697224ec5ba1835ffb7ddd76c08f2673aee3fc
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2026-02-10 13:44:45 +0300

    fix: fixing Book component

--- frontend/src/components/book/Book.tsx
+++ frontend/src/components/book/Book.tsx
@@ -1,8 +1,6 @@
 import { AiOutlineDelete } from 'react-icons/ai';
 import { BsBookmarkStarFill } from 'react-icons/bs';
 import { Button } from 'components/button/Button';
-import { useBook } from 'hooks/useBook';
-import { useFilters } from 'hooks/useFilters';
 import { highlightMatch } from 'utils/highlightMatch';
 import { BookType } from 'types';
 import s from './Book.module.css';
@@ -10,37 +8,54 @@ import s from './Book.module.css';
 type BookProps = {
   number: number;
   book: BookType;
+  deleteBook: (id: string) => void;
+  toggleFavorite: (id: string) => void;
+  filteredTitle: string;
+  filteredAuthor: string;
 };
 
-export const Book = ({ number, book }: BookProps) => {
-  const { removeBook, handleToggleFavorite } = useBook();
-  const { filteredTitle, filteredAuthor } = useFilters();
-
+export const Book = ({
+  number,
+  book,
+  deleteBook,
+  toggleFavorite,
+  filteredTitle,
+  filteredAuthor,
+}: BookProps) => {
   return (
     <div className={s.book}>
       <div className={s.content}>
         <span>{number}.</span>
-        <p>{highlightMatch(book.title, filteredTitle)}</p>
-        <strong>{highlightMatch(book.author, filteredAuthor)}</strong>
+        <p data-testid="bookTitle">
+          {highlightMatch(book.title, filteredTitle)}
+        </p>
+        <strong data-testid="bookAuthor">
+          {highlightMatch(book.author, filteredAuthor)}
+        </strong>
       </div>
 
       <div className={s.controls}>
         <Button
           btnText={
             <BsBookmarkStarFill
-              style={{ color: book.isFavorite ? 'yellow' : '' }}
+              style={{
+                color:
+                  book.isFavorite ? 'rgb(255, 255, 0)' : 'rgb(255, 255, 255)',
+              }}
             />
           }
           size="sm"
           variant="primary"
-          onClick={() => handleToggleFavorite(book.id)}
+          onClick={() => toggleFavorite(book.id)}
+          dataTestid="toggleFavoriteBtn"
         />
 
         <Button
           btnText={<AiOutlineDelete />}
           size="sm"
           variant="primary"
-          onClick={() => removeBook(book.id)}
+          onClick={() => deleteBook(book.id)}
+          dataTestid="deleteBookBtn"
         />
       </div>
     </div>
