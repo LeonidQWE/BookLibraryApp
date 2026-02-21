71f2fe1cc7e0be97e6fed8a4e9776418263be85a
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2026-02-21 13:14:17 +0300

    feat: add error case when fetchBook is error

--- frontend/src/redux/books/asyncActions/fetchBook.ts
+++ frontend/src/redux/books/asyncActions/fetchBook.ts
@@ -1,13 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { setError } from 'redux/error/errorSlice';
 import { BookByAPI } from 'types';
 
 export const fetchBook = createAsyncThunk<BookByAPI>(
   'books/fetchBook',
-  async () => {
-    const res = await axios.get<BookByAPI>('http://localhost:4000/random-book');
-    const bookFromAPI = res.data;
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get<BookByAPI>(
+        'http://localhost:4000/random-book'
+      );
+      const bookFromAPI = res.data;
 
-    return bookFromAPI;
+      return bookFromAPI;
+    } catch (error) {
+      thunkAPI.dispatch(setError(error.message));
+      throw error;
+    }
   }
 );
