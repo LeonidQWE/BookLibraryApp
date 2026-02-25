dbf41c14f6380938d65e1730b9faa06739966f2c
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2026-02-25 12:11:04 +0300

    feat: add delayed answer from api

--- api/index.js
+++ api/index.js
@@ -5,9 +5,20 @@ import bookData from './data/books.json' with { type: 'json' };
 const app = express();
 app.use(cors());
 
-app.get('/random-book', (req, res) => {
+const getRandomBook = () => {
   const randomIndex = Math.floor(Math.random() * bookData.length);
-  res.json(bookData[randomIndex]);
+  const randomBook =bookData[randomIndex];
+  return randomBook
+}
+
+app.get('/random-book', (req, res) => {
+  res.json(getRandomBook());
+});
+
+app.get('/random-book-delayed', (req, res) => {
+  setTimeout(() => {
+    res.json(getRandomBook());
+  }, 2000)
 });
 
 const port = process.env.PORT || 4000;
