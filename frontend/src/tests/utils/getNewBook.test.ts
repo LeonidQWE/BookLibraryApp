b84edb66b886cc01476cf225d188d091d4fc2906
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2026-02-26 18:15:48 +0300

    fix: fixing test for getNewBook util

--- frontend/src/tests/utils/getNewBook.test.ts
+++ frontend/src/tests/utils/getNewBook.test.ts
@@ -1,3 +1,4 @@
+import { BookType } from 'types';
 import { getNewBook } from 'utils/getNewBook';
 
 global.crypto.randomUUID = () => '123-123-123-123-354';
@@ -5,13 +6,16 @@ global.crypto.randomUUID = () => '123-123-123-123-354';
 describe('getNewBook', () => {
   it('should create new book', () => {
     const testBook = { title: 'Book Title', author: 'Book Author' };
-    const expectBook = {
+    const testSource = 'New Book';
+
+    const expectBook: BookType = {
       ...testBook,
       id: '123-123-123-123-354',
       isFavorite: false,
+      source: 'New Book',
     };
 
-    const book = getNewBook(testBook);
+    const book = getNewBook(testBook, testSource);
 
     expect(book).toEqual(expectBook);
   });
